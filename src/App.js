9dc70d77b46cb6919413779a1956a43ae52a1892
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-01-19 18:18:36 +0100

    Routes & redirects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Redirect } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
@@ -6,14 +7,22 @@ import Login from './pages/Login';
 import styles from './App.module.css';
 
 function App() {
+
+  const { user, authIsReady } = useAuthContext(); 
+
 	return (
 		<div className={styles.App}>
-			<Navbar />
+     {authIsReady && ( 
+       <>
+      <Navbar />
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/signup' element={<Signup />} />
-				<Route path='/login' element={<Login />} />
+				<Route path='/' element={user ? <Home /> : <Login/> } />
+				<Route path='/signup' element={!user ? <Signup /> : <Home />} />
+				<Route path='/login' element={!user ? <Login /> : <Home />} />
 			</Routes>
+      </>
+      )}
+			
 		</div>
 	);
 }
